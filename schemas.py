de3008bab7ee2e3e6c3272fadf88158a7f36977b
Alexis Piña <alexispina117@gmail.com>
2025-03-29 13:30:55 -0600

    Fixing error on card update schema

--- schemas.py
+++ schemas.py
@@ -2,7 +2,7 @@ from pydantic import BaseModel
 
 class CardUpdateRequest(BaseModel):
     """Request model for updating a card."""
-    title: str | None = None
+    titulo: str | None = None
     descripcion: str | None = None
     img: str | None = None
     tags: list[str] | None = None
