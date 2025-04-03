ce8cc076d1bf0d7f9a128e30c356727cd7d0f176
Alexis Piña <alexispina117@gmail.com>
2025-04-03 10:52:12 -0600

    Adding postgres db and migrations

--- utils/db.py
+++ utils/db.py
@@ -1,7 +1,8 @@
 from sqlmodel import create_engine, SQLModel
 
+from settings.base import DB_PORT, DB_HOST, DB_NAME, DB_USER, DB_PASSWORD
 
-DATABASE_URL = "sqlite:///database.db"
+DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 engine = create_engine(DATABASE_URL, echo=False)
 
-SQLModel.metadata.create_all(engine)
+#SQLModel.metadata.create_all(engine)
